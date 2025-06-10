ef90629b6c5bf622846c479054e516e457020a1f
God-Slayer-stack <muhammadindriawan34@gmail.com>
2025-06-10 23:43:34 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author USER
- */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek
+        Matematika indriawan = new Matematika(3,9);
+        
+        System.out.println("Hasil Penjumlahan: "+indriawan.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+indriawan.setPengurangan());
+        System.out.println("Hasil Perkalian: "+indriawan.setPerkalian());
+        System.out.println("Hasil Pembagian: "+indriawan.setPembagian());
+    }
     
 }
